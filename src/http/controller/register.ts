a14cea44be491b97eacf9d04983dd6c9598b22d6
Paulo <paulorfsantos17@gmail.com>
2024-07-03 13:34:50 -0300

    feat: add use-case register

---
+++ src/http/controller/register.ts
@@ -0,0 +1,23 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { registerUseCase } from '@/use-cases/register'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string().min(3).max(50),
+    email: z.string().email(),
+    password: z.string().min(8),
+  })
+
+  const { email, name, password } = registerBodySchema.parse(request.body)
+
+  try {
+    await registerUseCase({ email, name, password })
+  } catch (error) {
+    return reply.status(409).send()
+  }
+
+
+
+  return reply.status(201).send()
+}
