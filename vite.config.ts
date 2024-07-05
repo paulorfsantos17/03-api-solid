feba8bff4920c3a0e90fd53a15491db2c8ba0ded
Paulo <paulorfsantos17@gmail.com>
2024-07-05 18:36:13 -0300

    feat: add environment prisma

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ export default defineConfig({
   plugins: [tsConfigPaths()],
   test: {
     include: ['./src/**/*.spec.ts'],
+    environmentMatchGlobs: [
+      ['./src/http/controller/**.spec.ts', './src/vitest-environment/prisma'],
+    ],
   },
 })
