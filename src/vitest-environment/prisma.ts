51acaf4c9fbef56556bfd3b29e534d0af78d81d6
Paulo <paulorfsantos17@gmail.com>
2024-07-05 19:54:33 -0300

    chore: add environment prisma in vitest

--- src/vitest-environment/prisma.ts
+++ src/vitest-environment/prisma.ts
@@ -1,15 +1,42 @@
+import { env } from '@/env'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseUrl(schema: string) {
+  if (!env.DATABASE_URL) {
+    throw new Error('Missing DATABASE_URL environment variable')
+  }
+
+  const url = new URL(env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('Setup')
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseUrl(schema)
+
+    process.env.DATABASE_URL = databaseURL
+    execSync('pnpm prisma migrate deploy')
 
     return {
       async teardown() {
-        console.log('Teardown')
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
+
   transformMode: 'ssr',
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsConfigPaths()],
   test: {
-    include: ['./src/**/*.spec.ts'],
     environmentMatchGlobs: [
       ['./src/http/controller/**.spec.ts', './src/vitest-environment/prisma'],
     ],
