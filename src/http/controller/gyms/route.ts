373afd0f13b6bf5997a9de1a9264d594b51aec36
Paulo <paulorfsantos17@gmail.com>
2024-07-06 16:42:55 -0300

    fix: add '/' in routes gyms nearby

--- src/http/controller/gyms/route.ts
+++ src/http/controller/gyms/route.ts
@@ -8,7 +8,7 @@ export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
 
   app.get('/gyms/search', search)
-  app.get('gyms/nearby', nearby)
+  app.get('/gyms/nearby', nearby)
 
   app.post('/gyms/create', create)
 }
