586f2a0f7b729b66e867133ca06b68b5f9726b98
Paulo <paulorfsantos17@gmail.com>
2024-07-06 15:21:09 -0300

    feat: add controllers gyms

--- src/http/controller/gyms/route.ts
+++ src/http/controller/gyms/route.ts
@@ -1,6 +1,14 @@
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
+import { search } from './search'
+import { create } from './create'
+import { nearby } from './nearby'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
+
+  app.get('/gyms/search', search)
+  app.get('gyms/nearby', nearby)
+
+  app.post('/gyms/create', create)
 }
