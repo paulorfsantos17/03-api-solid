78d75b57c5c69eeaf2a990e2a40f42426e050736
Paulo <paulorfsantos17@gmail.com>
2024-07-09 16:28:43 -0300

    test: add create.spec.ts

---
+++ src/http/controller/check-ins/create.spec.ts
@@ -0,0 +1,37 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+describe('Create Check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to create a check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'JavaScript Gym',
+        latitude: -22.3685618,
+        longitude: -45.7762134,
+      },
+    }
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: -22.3685618,
+        longitude: -45.7762134,
+      })
+
+    expect(response.status).toBe(201)
+  })
+})
